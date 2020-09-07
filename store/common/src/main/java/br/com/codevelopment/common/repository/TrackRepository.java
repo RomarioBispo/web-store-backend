09517e38d4bd585eca688b4cd4ec05c73d38e4f3
RomarioBispo <romario.bsp.silva@gmail.com>
2020-09-07 10:10:20 -0300

    feat: criacao dos repositorios

---
+++ store/common/src/main/java/br/com/codevelopment/common/repository/TrackRepository.java
@@ -0,0 +1,10 @@
+package br.com.codevelopment.common.repository;
+
+import org.springframework.data.mongodb.repository.MongoRepository;
+import org.springframework.stereotype.Repository;
+
+import br.com.codevelopment.common.domain.model.Track;
+
+@Repository
+public interface TrackRepository extends MongoRepository<Track, String> {
+}
