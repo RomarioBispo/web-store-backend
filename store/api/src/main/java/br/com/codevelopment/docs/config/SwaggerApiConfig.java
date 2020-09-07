a5f0e0674ac3dbb51b24a2965e602105870615ce
RomarioBispo <romario.bsp.silva@gmail.com>
2020-09-07 09:44:18 -0300

    feat: correcao do swagger das apis

--- store/api/src/main/java/br/com/codevelopment/docs/config/SwaggerApiConfig.java
+++ store/api/src/main/java/br/com/codevelopment/docs/config/SwaggerApiConfig.java
@@ -3,13 +3,8 @@ package br.com.codevelopment.docs.config;
 import org.springframework.context.annotation.Configuration;
 
 import br.com.codevelopment.common.docs.BaseSwaggerConfig;
-import springfox.documentation.swagger2.annotations.EnableSwagger2;
 
 @Configuration
-@EnableSwagger2
 public class SwaggerApiConfig extends BaseSwaggerConfig{
 
-	public SwaggerApiConfig() {
-		super("br.com.codevelopment.api");
-	}
 }
